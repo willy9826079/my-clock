28d96d5d4dfcb67fddf6cea7a25e67d7086b33a6
willy9826079 <9826079@gmail.com>
2015-02-09 16:52:27 +0800

    first buILD

---
+++ ksana.js
@@ -0,0 +1,40 @@
+jsonp_handler({
+ "version": "1",
+ "build": 3,
+ "title": "my-clock",
+ "minruntime": 1,
+ "baseurl": "http://willy9826079@rawgit.com/willy9826079/my-clock/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "index.css",
+  "bundle.js",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  157,
+  64,
+  155656,
+  277980,
+  645243,
+  744,
+  35607,
+  113502
+ ],
+ "filedates": [
+  "2015-02-09T08:44:46.302Z",
+  "2015-02-09T08:23:58.269Z",
+  "2015-02-09T08:44:54.972Z",
+  "2015-02-09T08:23:58.274Z",
+  "2015-02-09T08:23:58.281Z",
+  "2015-02-09T08:44:07.473Z",
+  "2015-02-09T08:23:58.264Z",
+  "2015-02-09T08:23:58.263Z"
+ ],
+ "date": "2015-02-09T08:44:55.932Z"
+})
